863d0bb489a49c508290d5fb663d9f27827d77eb
Pascal Muchiri <pascalmuchiri@gmail.com>
2025-12-26 22:08:19 +0300

    feat: Integrate Sentry for error monitoring and performance tracing.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -60,5 +61,5 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->withoutOverlapping();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
